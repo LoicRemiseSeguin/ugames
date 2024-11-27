0fba3419021594d7e056c2ec53b70f16a330f824
Loïc REMISE SEGUIN <lremise@cirilgroup.com>
2024-11-27 16:30:10 +0100

    add

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 app.use('/api', routes);
